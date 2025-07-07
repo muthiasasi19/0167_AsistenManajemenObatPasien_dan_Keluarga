5e08b615e07d1286003547a0bd7868889014532c
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-07 19:23:27 +0700

    Membuat Class untuk mengkoneksikan pasien dengan dokter

--- lib/data/models/request/connect_patient_doctor_request_model.dart
+++ lib/data/models/request/connect_patient_doctor_request_model.dart
@@ -1,1 +1,18 @@
+import 'dart:convert';
 
+class ConnectPatientRequestModel {
+  final String patientUniqueId; // ID unik pasien yang akan dikoneksikan
+  final String doctorId; // ID dokter yang melakukan koneksi
+
+  ConnectPatientRequestModel({
+    required this.patientUniqueId,
+    required this.doctorId,
+  });
+
+  String toJson() => json.encode(toMap());
+
+  Map<String, dynamic> toMap() => {
+    "patientUniqueId": patientUniqueId,
+    "doctorId": doctorId,
+  };
+}
