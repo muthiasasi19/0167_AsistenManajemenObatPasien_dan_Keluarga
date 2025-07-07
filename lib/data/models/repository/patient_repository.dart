877835a49ab21b66724a652e113d001c9ab3d848
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-07 19:27:17 +0700

    Membuat class PatientRepository

--- lib/data/models/repository/patient_repository.dart
+++ lib/data/models/repository/patient_repository.dart
@@ -1,1 +1,15 @@
+import 'dart:convert';
+import 'dart:developer' as developer;
+import 'package:dartz/dartz.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:manajemen_obat/service/service_http_client.dart';
+import 'package:manajemen_obat/data/models/response/patient_response_model.dart';
+import 'package:manajemen_obat/data/models/response/patient_search_result_model.dart';
+import 'package:manajemen_obat/data/models/response/doctor_response_model.dart';
 
+class PatientRepository {
+  final ServiceHttpClient _httpClient;
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
+
+  PatientRepository(this._httpClient);
+}
