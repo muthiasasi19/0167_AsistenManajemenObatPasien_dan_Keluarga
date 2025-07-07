a19231b83f195ee4b30ffeb2add4d9a801de84b5
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-07 19:55:39 +0700

    Hapus / yang tidak digunakan

--- lib/data/models/request/connect_patient_family_request_model.dart
+++ lib/data/models/request/connect_patient_family_request_model.dart
@@ -3,16 +3,11 @@ import 'dart:convert';
 class FamilyConnectRequestModel {
   final String patientUniqueId;
 
-  FamilyConnectRequestModel({
-    required this.patientUniqueId, /
-  });
+  FamilyConnectRequestModel({required this.patientUniqueId});
 
   // Metode untuk mengkonversi objek menjadi String JSON
   String toJson() => json.encode(toMap());
 
   // Metode untuk mengkonversi objek menjadi Map<String, dynamic>
-  Map<String, dynamic> toMap() => {
-    "patientUniqueId": patientUniqueId,
-  };
+  Map<String, dynamic> toMap() => {"patientUniqueId": patientUniqueId};
 }
-
