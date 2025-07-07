e1f80dae93c46ef0ed1f64e6792dbef25685e34a
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-07 17:38:59 +0700

    Membuat class RegisterPage dengan statefulWidget

--- lib/presentation/auth/register_screen.dart
+++ lib/presentation/auth/register_screen.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:manajemen_obat/data/models/request/register_request_model.dart';
+import 'package:manajemen_obat/presentation/auth/bloc/register/register_bloc.dart';
+import 'package:manajemen_obat/core/components/spaces.dart';
+import 'package:manajemen_obat/core/core.dart';
+import 'package:manajemen_obat/presentation/auth/login_screen.dart';
 
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {}
