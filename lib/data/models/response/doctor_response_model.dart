fe5ac3d54d2c9c6bdb9d738523f293ca9588d0c7
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-07 18:53:42 +0700

    Buat class DoctorData lengkap dengan parsing dari dan ke JSON

--- lib/data/models/response/doctor_response_model.dart
+++ lib/data/models/response/doctor_response_model.dart
@@ -1,1 +1,31 @@
+class DoctorData {
+  final String idDokter;
+  final String name;
+  final String specialization;
+  final String phoneNumber;
 
+  DoctorData({
+    required this.idDokter,
+    required this.name,
+    required this.specialization,
+    required this.phoneNumber,
+  });
+
+  factory DoctorData.fromJson(Map<String, dynamic> json) {
+    return DoctorData(
+      idDokter: json['id_dokter'] ?? '',
+      name: json['nama'] ?? 'Nama Dokter Tidak Diketahui',
+      specialization: json['specialization'] ?? 'Spesialisasi Tidak Tersedia',
+      phoneNumber: json['nomor_telepon'] ?? 'Tidak Tersedia',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id_dokter': idDokter,
+      'nama': name,
+      'specialization': specialization,
+      'nomor_telepon': phoneNumber,
+    };
+  }
+}
