8438b195fdf1bc34959bc848623b7bd4650e9811
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-07 18:05:11 +0700

    Membuat clas LoginScreen dengan StatefulWidget

--- lib/presentation/auth/login_screen.dart
+++ lib/presentation/auth/login_screen.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:manajemen_obat/data/models/request/login_request_model.dart';
+import 'package:manajemen_obat/presentation/Home/keluarga_homescreen.dart';
+import 'package:manajemen_obat/presentation/Home/pasien_homescreen.dart';
+import 'package:manajemen_obat/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:manajemen_obat/presentation/auth/register_screen.dart';
+import 'package:manajemen_obat/core/components/spaces.dart';
+import 'package:manajemen_obat/core/core.dart';
+import 'package:manajemen_obat/presentation/Home/dokter_homescreen.dart';
+import 'package:flutter/gestures.dart';
 
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
