dec7f4e8e33000757a4072203c8b4538d00003e4
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-07 19:24:40 +0700

    Membuat class untuk mengkoneksikan pasien dengan keluarga

--- lib/data/models/request/connect_patient_family_request_model.dart
+++ lib/data/models/request/connect_patient_family_request_model.dart
@@ -1,1 +1,18 @@
+import 'dart:convert';
+
+class FamilyConnectRequestModel {
+  final String patientUniqueId;
+
+  FamilyConnectRequestModel({
+    required this.patientUniqueId, /
+  });
+
+  // Metode untuk mengkonversi objek menjadi String JSON
+  String toJson() => json.encode(toMap());
+
+  // Metode untuk mengkonversi objek menjadi Map<String, dynamic>
+  Map<String, dynamic> toMap() => {
+    "patientUniqueId": patientUniqueId,
+  };
+}
 
