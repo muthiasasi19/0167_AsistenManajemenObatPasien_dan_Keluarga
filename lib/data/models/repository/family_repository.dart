6fa5bec95ff6b03e3d05a6106d5cfde9eabcb5d3
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-07 19:09:48 +0700

    Membuat class FamilyRepository

--- lib/data/models/repository/family_repository.dart
+++ lib/data/models/repository/family_repository.dart
@@ -1,1 +1,20 @@
+// lib/data/models/repository/family_repository.dart
+import 'dart:convert';
+import 'dart:developer';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:manajemen_obat/service/service_http_client.dart';
+import 'package:manajemen_obat/data/models/response/connect_patient_family_response_model.dart';
+import 'package:manajemen_obat/data/models/response/medication_response_model.dart';
+import 'package:manajemen_obat/data/models/response/medication_history_response_model.dart';
+import 'package:manajemen_obat/data/models/response/patient_location_response_model.dart';
+import 'package:manajemen_obat/data/models/request/connect_patient_family_request_model.dart';
 
+class FamilyRepository {
+  final ServiceHttpClient _httpClient;
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
+
+  FamilyRepository(this._httpClient);
+
+  // Helper untuk mendapatkan ID internal keluarga dari local storage
+}
