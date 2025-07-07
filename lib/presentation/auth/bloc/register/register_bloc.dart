4713829b80ca3059250e8a558602b5560ba4fd86
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-07 18:31:57 +0700

    Membuat bloc untuk Register

--- lib/presentation/auth/bloc/register/register_bloc.dart
+++ lib/presentation/auth/bloc/register/register_bloc.dart
@@ -1,1 +1,35 @@
+import 'package:bloc/bloc.dart';
+import 'package:manajemen_obat/data/models/request/register_request_model.dart';
+import 'package:manajemen_obat/data/models/repository/auth_repository.dart';
+import 'package:flutter/foundation.dart';
 
+part 'register_event.dart';
+part 'register_state.dart';
+
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  final AuthRepository authRepository;
+
+  RegisterBloc({required this.authRepository}) : super(RegisterInitial()) {
+    on<RegisterRequested>(_onRegisterRequested);
+  }
+
+  Future<void> _onRegisterRequested(
+    RegisterRequested event,
+    Emitter<RegisterState> emit,
+  ) async {
+    emit(RegisterLoading());
+
+    final result = await authRepository.register(event.request);
+
+    result.fold(
+      (l) {
+        debugPrint('Register Failure: $l');
+        emit(RegisterFailure(error: l));
+      },
+      (r) {
+        debugPrint('Register Success: $r');
+        emit(RegisterSuccess(message: r));
+      },
+    );
+  }
+}
--- lib/presentation/auth/bloc/register/register_event.dart
+++ lib/presentation/auth/bloc/register/register_event.dart
@@ -1,1 +1,9 @@
+part of 'register_bloc.dart';
 
+sealed class RegisterEvent {}
+
+class RegisterRequested extends RegisterEvent {
+  final RegisterRequestModel request;
+
+  RegisterRequested({required this.request});
+}
