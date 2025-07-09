bb0def0b0180dadc0788bf25d591ecf7806a2e25
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-09 21:31:34 +0700

    Membuat class PatientLocationResponseModel dan PatientLocationData

--- lib/data/models/response/patient_location_response_model.dart
+++ lib/data/models/response/patient_location_response_model.dart
@@ -1,1 +1,50 @@
+import 'dart:convert';
 
+class PatientLocationResponseModel {
+  final String? message;
+  final PatientLocationData? data;
+
+  PatientLocationResponseModel({this.message, this.data});
+
+  factory PatientLocationResponseModel.fromJson(String str) =>
+      PatientLocationResponseModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory PatientLocationResponseModel.fromMap(Map<String, dynamic> json) =>
+      PatientLocationResponseModel(
+        message: json["message"],
+        data:
+            json["data"] == null
+                ? null
+                : PatientLocationData.fromMap(json["data"]),
+      );
+
+  Map<String, dynamic> toMap() => {"message": message, "data": data?.toMap()};
+}
+
+class PatientLocationData {
+  final double? latitude;
+  final double? longitude;
+  final String? timestamp;
+
+  PatientLocationData({this.latitude, this.longitude, this.timestamp});
+
+  factory PatientLocationData.fromJson(String str) =>
+      PatientLocationData.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory PatientLocationData.fromMap(Map<String, dynamic> json) =>
+      PatientLocationData(
+        latitude: (json["latitude"] as num?)?.toDouble(),
+        longitude: (json["longitude"] as num?)?.toDouble(),
+        timestamp: json["timestamp"] as String?,
+      );
+
+  Map<String, dynamic> toMap() => {
+    "latitude": latitude,
+    "longitude": longitude,
+    "timestamp": timestamp,
+  };
+}
