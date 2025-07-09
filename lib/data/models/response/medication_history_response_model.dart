b08b65947a42e7726089eac60321b7c833cab764
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-09 15:05:31 +0700

    Menambahkan model response MedicationHistoryResponseModel untuk memuat data riwayat konsumsi obat dari JSON

--- lib/data/models/response/medication_history_response_model.dart
+++ lib/data/models/response/medication_history_response_model.dart
@@ -1,1 +1,31 @@
+import 'dart:convert';
+import 'dart:developer' as developer;
+import 'package:manajemen_obat/data/models/response/medication_response_model.dart';
 
+class MedicationHistoryResponseModel {
+  final String? message;
+  final List<MedicationHistoryData> data;
+
+  MedicationHistoryResponseModel({this.message, required this.data});
+
+  factory MedicationHistoryResponseModel.fromJson(String str) =>
+      MedicationHistoryResponseModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory MedicationHistoryResponseModel.fromMap(Map<String, dynamic> json) =>
+      MedicationHistoryResponseModel(
+        message: json["message"],
+        data: List<MedicationHistoryData>.from(
+          (json["data"] as List<dynamic>?)?.map(
+                (x) => MedicationHistoryData.fromMap(x as Map<String, dynamic>),
+              ) ??
+              [],
+        ),
+      );
+
+  Map<String, dynamic> toMap() => {
+    "message": message,
+    "data": List<dynamic>.from(data.map((x) => x.toMap())),
+  };
+}
