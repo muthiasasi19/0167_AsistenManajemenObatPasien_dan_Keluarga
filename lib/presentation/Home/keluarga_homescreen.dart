ced30de04affbd0a582e638379d326abbd8b190d
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-09 10:35:52 +0700

    Membuat class kelaurgaHomeScreen

--- lib/presentation/Home/keluarga_homescreen.dart
+++ lib/presentation/Home/keluarga_homescreen.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:intl/intl.dart';
+import 'dart:developer';
+import 'package:manajemen_obat/core/core.dart';
+import 'package:manajemen_obat/data/models/response/connect_patient_family_response_model.dart';
+import 'package:manajemen_obat/presentation/auth/login_screen.dart';
+import 'package:manajemen_obat/presentation/profil/keluarga_profil_screen.dart';
+import 'package:manajemen_obat/data/models/response/login_response_model.dart';
+import 'package:manajemen_obat/presentation/family/bloc/family_bloc.dart';
+import 'package:manajemen_obat/presentation/Home/medication_page.dart';
+import 'package:manajemen_obat/presentation/Home/pasien_location_page.dart';
 
+class KeluargaHomeScreen extends StatefulWidget {
+  const KeluargaHomeScreen({super.key});
+
+  @override
+  State<KeluargaHomeScreen> createState() => _KeluargaHomeScreenState();
+}
