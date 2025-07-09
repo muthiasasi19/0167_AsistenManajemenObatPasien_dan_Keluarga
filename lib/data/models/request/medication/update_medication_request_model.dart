6f4355a997684080b361d7ce7ca8e0532893d21a
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-09 11:35:18 +0700

    Tambah model request untuk update data obat

--- lib/data/models/request/medication/update_medication_request_model.dart
+++ lib/data/models/request/medication/update_medication_request_model.dart
@@ -1,1 +1,29 @@
+import 'dart:convert';
 
+import 'package:manajemen_obat/data/models/response/medication_response_model.dart';
+
+class UpdateMedicationRequestModel {
+  final String medicationName;
+  final String dosage;
+  final MedicationSchedule schedule;
+  final String? description;
+  final String? photoUrl;
+
+  UpdateMedicationRequestModel({
+    required this.medicationName,
+    required this.dosage,
+    required this.schedule,
+    this.description,
+    this.photoUrl,
+  });
+
+  String toJson() => json.encode(toMap());
+
+  Map<String, dynamic> toMap() => {
+    "medicationName": medicationName,
+    "dosage": dosage,
+    "schedule": schedule.toMap(),
+    "description": description,
+    "photoUrl": photoUrl,
+  };
+}
