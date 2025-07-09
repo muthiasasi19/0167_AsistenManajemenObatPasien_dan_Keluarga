8470ee8736dec2c17bc33c735add3aafd818e6d4
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-09 10:34:19 +0700

    Membuat Class data untuk koneksi pasien dan keluarga

--- lib/data/models/response/connect_patient_family_response_model.dart
+++ lib/data/models/response/connect_patient_family_response_model.dart
@@ -29,3 +29,49 @@ class FamilyConnectedPatientResponseModel {
     "data": List<dynamic>.from(data.map((x) => x.toMap())),
   };
 }
+
+class FamilyConnectedPatientData {
+  final int? idGlobal;
+  final String? idUnik;
+  final String? nama;
+  final String? tanggalLahir;
+  final String? jenisKelamin;
+  final String? nomorTelepon;
+  final String? alamat;
+
+  FamilyConnectedPatientData({
+    this.idGlobal,
+    this.idUnik,
+    this.nama,
+    this.tanggalLahir,
+    this.jenisKelamin,
+    this.nomorTelepon,
+    this.alamat,
+  });
+
+  factory FamilyConnectedPatientData.fromJson(String str) =>
+      FamilyConnectedPatientData.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory FamilyConnectedPatientData.fromMap(Map<String, dynamic> json) =>
+      FamilyConnectedPatientData(
+        idGlobal: json["idGlobal"] as int?,
+        idUnik: json["idUnik"] as String?,
+        nama: json["nama"] as String?,
+        tanggalLahir: json["tanggalLahir"] as String?,
+        jenisKelamin: json["jenisKelamin"] as String?,
+        nomorTelepon: json["nomorTelepon"] as String?,
+        alamat: json["alamat"] as String?,
+      );
+
+  Map<String, dynamic> toMap() => {
+    "idGlobal": idGlobal,
+    "idUnik": idUnik,
+    "nama": nama,
+    "tanggalLahir": tanggalLahir,
+    "jenisKelamin": jenisKelamin,
+    "nomorTelepon": nomorTelepon,
+    "alamat": alamat,
+  };
+}
