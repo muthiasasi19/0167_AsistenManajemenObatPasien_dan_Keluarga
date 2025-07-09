467c43ef36ae4fa887d8f2b8aa6a606a5ad58802
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-09 10:24:23 +0700

    Membuat Class untuk koneksi pasien dan keluarga

--- lib/data/models/response/connect_patient_family_response_model.dart
+++ lib/data/models/response/connect_patient_family_response_model.dart
@@ -1,1 +1,31 @@
+import 'dart:convert';
 
+class FamilyConnectedPatientResponseModel {
+  final String? message;
+  final List<FamilyConnectedPatientData> data;
+
+  FamilyConnectedPatientResponseModel({this.message, required this.data});
+
+  factory FamilyConnectedPatientResponseModel.fromJson(String str) =>
+      FamilyConnectedPatientResponseModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory FamilyConnectedPatientResponseModel.fromMap(
+    Map<String, dynamic> json,
+  ) => FamilyConnectedPatientResponseModel(
+    message: json["message"],
+    data: List<FamilyConnectedPatientData>.from(
+      (json["data"] as List<dynamic>?)?.map(
+            (x) =>
+                FamilyConnectedPatientData.fromMap(x as Map<String, dynamic>),
+          ) ??
+          [],
+    ),
+  );
+
+  Map<String, dynamic> toMap() => {
+    "message": message,
+    "data": List<dynamic>.from(data.map((x) => x.toMap())),
+  };
+}
