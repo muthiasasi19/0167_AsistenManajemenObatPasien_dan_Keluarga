00330ae32c6e8e12d288fb38cd00b5fb25168331
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-07-09 11:30:23 +0700

    Model data tambah obat termasuk dosis, jadwal, dan gambar/foto

--- lib/data/models/request/medication/add_medication_request_model.dart
+++ lib/data/models/request/medication/add_medication_request_model.dart
@@ -1,1 +1,42 @@
+import 'dart:convert';
+import 'dart:io';
 
+import 'package:manajemen_obat/data/models/response/medication_response_model.dart';
+
+class AddMedicationRequestModel {
+  final String medicationName;
+  final String dosage;
+  final MedicationSchedule schedule;
+  final String? description;
+  final String? photoUrl;
+  final File? photoFile;
+
+  AddMedicationRequestModel({
+    required this.medicationName,
+    required this.dosage,
+    required this.schedule,
+    this.description,
+    this.photoUrl,
+    this.photoFile,
+  });
+
+  String toJson() => json.encode(toMap());
+
+  Map<String, dynamic> toMap() => {
+    "medicationName": medicationName,
+    "dosage": dosage,
+    "schedule": schedule.toMap(),
+    "description": description,
+    "photoUrl": photoUrl,
+  };
+
+  @override
+  List<Object?> get props => [
+    medicationName,
+    dosage,
+    schedule,
+    description,
+    photoUrl,
+    photoFile,
+  ];
+}
